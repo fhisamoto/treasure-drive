699bde67af4193521636b8ba58ce4bea8442eef0
Fabio Hisamoto <fhisamoto@gmail.com>
2013-07-27 01:25:08 -0300

    Google drive config.

--- treasure_drive.rb
+++ treasure_drive.rb
@@ -1,6 +1,12 @@
 class TreasureDrive < Sinatra::Application
   register Sinatra::Async
 
+  def self.config
+    config_file = File.read(File.join( File.dirname(__FILE__), 'config', 'google_drive.yml.erb'))
+    @config ||= YAML.load(ERB.new(config_file).result)['production']
+    @config
+  end
+
   get '/' do
     "Hello"
   end
