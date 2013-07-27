ed269c033404517b7f7307e436828d0eb1715b97
Fabio Hisamoto <fhisamoto@gmail.com>
2013-07-27 04:33:12 -0300

    Module CopyToGoogleDrive created.

---
+++ copy_to_google_drive.rb
@@ -0,0 +1,39 @@
+module CopyToGoogleDrive
+  def get_files
+    session = google_drive_session
+    urls.each_pair do |name, url|
+      content = donwload_ssl_verify_none(url)
+      session.upload_from_string(content, name, :content_type =>  "application/vnd.ms-excel")
+    end
+  end
+
+  def google_drive_session
+    @google_drive_session ||= GoogleDrive.login(google_drive_config['email'], google_drive_config['password'])
+  end
+
+  def google_drive_config
+    erb = File.join(File.dirname(__FILE__), 'config', 'google_drive.yml.erb')
+    @google_drive_config ||= YAML.load(ERB.new(File.read(erb)).result)['production']
+  end
+
+  def urls
+    @urls ||= {
+      'lft' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/LFT_2013.xls',
+      'ltn' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/LTN_2013.xls',
+      'ntn-c' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/NTN-C_2013.xls',
+      'ntn-b' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/NTN-B_2013.xls',
+      'ntn-b_principal' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/NTN-B_Principal_2013.xls',
+      'ntf' => 'https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/NTN-F_2013.xls'
+    }
+  end
+
+  def donwload_ssl_verify_none(url)
+    uri = URI(url)
+    req = Net::HTTP::Get.new(uri.path)
+    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https',
+                                              :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+      https.request(req)
+    end
+    res.body
+  end
+end
