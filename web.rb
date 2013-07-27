198a1fe55c708ce5d9a1b160200150e620ee879d
Fabio Hisamoto <fhisamoto@gmail.com>
2013-07-27 19:16:34 -0300

    Prints ntn-b info.

--- web.rb
+++ web.rb
@@ -1,7 +1,13 @@
+require './treasure_drive'
+
 class Web < Sinatra::Application
   register Sinatra::Async
 
+  include TreasureDrive
+
   get '/' do
-    "Hello"
+    ntn_b_file = google_drive_session.file_by_title('ntn-b')
+    s = Roo::Google.new(ntn-b.key, :user => google_drive_config['email'], :password => google_drive_config['password'])
+    s.info
   end
 end
