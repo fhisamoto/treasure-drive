300c813d345de93f9072e07762cebde77259b084
Fabio Hisamoto <fhisamoto@gmail.com>
2013-07-27 19:21:28 -0300

    Prints tables.

--- web.rb
+++ web.rb
@@ -7,7 +7,7 @@ class Web < Sinatra::Application
 
   get '/' do
     ntn_b_file = google_drive_session.file_by_title('ntn-b')
-    s = Roo::Google.new(ntn-b.key, :user => google_drive_config['email'], :password => google_drive_config['password'])
-    s.info
+    s = google_drive_session.spreadsheet_by_key(ntn_b_file.key)
+    s.tables.join(" ")
   end
 end
